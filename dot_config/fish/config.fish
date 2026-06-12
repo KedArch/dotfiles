98127fe4f5486ce1f3bebca71da24140686074b2
Radosław Woźniak <radoslaw.wozniak2000@gmail.com>
2026-06-12 14:56:47 +0200

    config.fish - fix ssh agent condition

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
   set -g fish_greeting
   set -q SSH_AUTH_SOCK; or set -gx SSH_AUTH_SOCK "$HOME/.ssh/agent.sock"
-  if test -z "$SSH_AUTH_SOCK"
+  if not test -S "$SSH_AUTH_SOCK"
     ssh-agent -a "$SSH_AUTH_SOCK" > /dev/null
   end
 end
